f08af217329e7179d4b22ecb86b242007c0d554f
Suthakar R <146537444+suthakarnrs@users.noreply.github.com>
2024-02-26 19:35:43 +0530

    Add files via upload

---
+++ Bulb/app.js
@@ -0,0 +1,14 @@
+
+var on_btn = document.getElementById("on");
+var off_buton =document.getElementById("off");
+var img = document.getElementById("bulb");
+function bulb_on(){
+img.src ="./images/bulb on.jpg";
+
+
+}
+
+function bulb_off(){
+img.src="./images/off.jpg";
+
+}
